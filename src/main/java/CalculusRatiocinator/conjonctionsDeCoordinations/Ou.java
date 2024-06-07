18da9de3f089c69236e3d4c42b95f68f4ede919c
Sarobidy <sarobidy.rakotonarivo.1@gmail.com>
2024-06-07 16:22:37 +0300

    feat: ajout du conjonction 'Ou'

---
+++ src/main/java/CalculusRatiocinator/conjonctionsDeCoordinations/Ou.java
@@ -0,0 +1,23 @@
+package main.java.CalculusRatiocinator.conjonctionsDeCoordinations;
+
+import main.java.CalculusRatiocinator.Affirmation;
+
+import java.util.Set;
+
+public class Ou extends Affirmation {
+    private final String affirmation1;
+    private final String affirmation2;
+    protected Ou(Set<String> affirmationsVraies, Set<String> affirmationsFausses, String affirmation1, String affirmation2) {
+        super(affirmationsVraies, affirmationsFausses);
+        this.affirmation1 = affirmation1;
+        this.affirmation2 = affirmation2;
+    }
+
+    @Override
+    public boolean estVraie() {
+        if (estVraie(affirmation1) || estVraie(affirmation2)){
+            return true;
+        }
+        return false;
+    }
+}
